5171325b27e5d9c0774a0e947737d46b1768647f
rafalb8 <R@falek3#>
2019-10-10 22:21:05 +0200

    Handling ValueError while generating title

--- gui/Actions.py
+++ gui/Actions.py
@@ -194,22 +194,25 @@ class Actions:
 
         title = self.info['title'] + ' ' + volume
 
-        # If chapters are not from the same volume
-        if volume == 'Chapters:':
-            numbers = [int(float(''.join(s for s in name if s.isdigit() or s == '.' or s == '-'))) for name in selected]
-            numbers = list(set(numbers))
+        try:
+            # If chapters are not from the same volume
+            if volume == 'Chapters:':
+                numbers = [int(float(''.join(s for s in name if s.isdigit() or s == '.' or s == '-'))) for name in selected]
+                numbers = list(set(numbers))
 
-            # Generate ranges for number list
-            ranges = Utils.ranges(numbers)
+                # Generate ranges for number list
+                ranges = Utils.ranges(numbers)
 
-            for r in ranges:
-                if r[0] == r[1]:
-                    title += ' {0},'.format(r[0])
-                else:
-                    title += ' {0} - {1},'.format(r[0], r[1])
+                for r in ranges:
+                    if r[0] == r[1]:
+                        title += ' {0},'.format(r[0])
+                    else:
+                        title += ' {0} - {1},'.format(r[0], r[1])
 
-            if title[-1] == ',':
-                title = title[:-1]
+                if title[-1] == ',':
+                    title = title[:-1]
+        except ValueError:
+            pass
 
         # Show Title Input Dialog
         dlg = QInputDialog()
